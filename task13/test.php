5877e502969a860b60b5e8f06409a125f0a7b1c8
Zhukov <zavix1988@gmail.com>
2019-05-24 10:35:28 +0300

    task13

---
+++ task13/test.php
@@ -0,0 +1,130 @@
+<?php
+
+
+require 'Calc.php';
+
+ 
+class CalculatorTests extends \PHPUnit_Framework_TestCase
+{
+    private $calc;
+ 
+    protected function setUp()
+    {
+        $this->calc = new Calc();
+    }
+ 
+    protected function tearDown()
+    {
+        $this->calculator = NULL;
+    }
+
+    public function testSetFirstVar()
+    {
+        $this->calc->setFirstVar(-5);
+        $result = $this->calc->getFirstVar();
+        $this->assertEquals(-5, $result);
+    }
+
+    public function testSetSecondVar()
+    {
+        $this->calc->setSecondVar(3);
+        $result = $this->calc->getSecondVar();
+        $this->assertEquals(3, $result);
+    }
+ 
+    public function testAddVars()
+    {
+        $this->calc->setFirstVar(-5);
+        $this->calc->setSecondVar(3);
+        $result = $this->calc->addVars();
+        $this->assertEquals(-2, $result);
+    }
+
+    public function testSubstructVars()
+    {
+        $this->calc->setFirstVar(-5);
+        $this->calc->setSecondVar(3);
+        $result = $this->calc->substructVars();
+        $this->assertEquals(-8, $result);
+    }
+
+    public function testMultiplicateVars()
+    {
+        $this->calc->setFirstVar(-5);
+        $this->calc->setSecondVar(3);
+        $result = $this->calc->multiplicateVars();
+        $this->assertEquals(-15, $result);
+    }
+
+    public function testDivizVars()
+    {
+        $this->calc->setFirstVar(-15);
+        $this->calc->setSecondVar(3);
+        $result = $this->calc->DivizVars();
+        $this->assertEquals(-5, $result);
+    }
+
+    public function testNegateVar()
+    {
+        $this->calc->setFirstVar(-5);
+        $result = $this->calc->negateVar();
+        $this->assertEquals(5, $result);
+    }
+
+    public function testSqrtVar()
+    {
+        $this->calc->setFirstVar(121);
+        $result = $this->calc->sqrtVar();
+        $this->assertEquals(11, $result);
+    }
+
+    public function testOneDivizVar()
+    {
+        $this->calc->setFirstVar(2);
+        $result = $this->calc->oneDivizVar();
+        $this->assertEquals(0.5, $result);
+    }
+
+    public function testClearVars()
+    {
+        $this->calc->setFirstVar(3);
+        $this->calc->setSecondVar(-5);
+        $this->calc->clearVars();
+        $this->assertEquals(NULL, $this->calc->getFirstVar());
+        $this->assertEquals(NULL, $this->calc->getSecondVar());
+    }
+
+    public function testMemoryAdd()
+    {
+        $this->calc->setFirstVar(3);
+        $this->calc->memoryAdd();
+        $this->calc->memoryAdd();
+        $result = $this->calc->memoryGet();
+        $this->assertEquals(6, $result);
+
+    }
+
+    public function testMemorySubstruct()
+    {
+        $this->calc->setFirstVar(6);
+        $this->calc->memoryAdd();
+        $this->calc->setFirstVar(3);
+        $this->calc->memorySubstruct();
+        $result = $this->calc->memoryGet();
+        $this->assertEquals(3, $result);
+
+    }
+
+    public function testClearMemory()
+    {
+        $this->calc->setFirstVar(6);
+        $this->calc->memoryAdd();
+        $this->calc->setFirstVar(3);
+        $this->calc->memorySubstruct();
+        $this->calc->clearMemory();
+        $result = $this->calc->memoryGet();
+        $this->assertEquals(NULL, $result);
+
+    }
+
+}
