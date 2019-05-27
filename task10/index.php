c14ad4150497eae5775c8bea335fae87b3fe40f7
Zhukov <zavix1988@gmail.com>
2019-05-27 17:11:35 +0300

    task10 add select

--- task10/index.php
+++ task10/index.php
@@ -15,24 +15,35 @@ include 'libs/SQLPDO.php';
 
 
 $bazadan = new SQLPDO();
-$bazadan->setTable('books');
-var_dump($bazadan->setTable('books')->setField(['name', 'slug']));
+
+/*SELECT books.id, books.name, books.slug, books.price, books.pubyear, books.lang, books.description FROM books 
+            LEFT JOIN book_author ON books.id=book_author.book_id 
+            LEFT JOIN book_genre ON books.id=book_genre.book_id 
+        WHERE book_author.author_id = 13 OR book.name LIKE '%book' 
+        GROUP BY books.id";*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
+      
+        
+        
+var_dump($bazadan->setTable('books')
+                    ->setTable('book_author')
+                    ->setTable('book_genre')
+                        ->setField('books', 'id' )
+                        ->setField('books', 'name' )
+                        ->setField('books', 'slug' )
+                        ->setField('books', 'price' )
+                        ->setField('books', 'pubyear' )
+                        ->setField('books', 'lang' )
+                        ->setField('books', 'description' )
+                        ->setField('book_author', 'author_id' )
+                            ->setJoin('INNER JOIN', 'books', 'id', 'book_author','book_id')
+                            ->setJoin('INNER JOIN', 'books', 'id', 'book_genre', 'book_id')
+                                ->setWhere('author_id', '=', '12', false, 'book_author')
+                                    ->setGroupBy('books', 'id')
+                                        ->select()
+                        );
 
 
 
--- task10/libs/ParentSQL.php
+++ task10/libs/ParentSQL.php
@@ -8,52 +8,77 @@
 
 class ParentSQL
 {
-    protected $table;
+    protected $tables;
     protected $fields;
-    protected $query;
-    protected $errors;
-    protected $limit;
-    protected $values;
+    protected $joins;
     protected $conditions;
+    protected $group;
+    protected $query;
 
-    protected function __construct()
-    {
-        $this->table = '';
+    public function __construct() {
+        $this->tables = [];
         $this->fields = [];
-        $this->values = [];
+        $this->joins = [];
+        $this->conditions = [];
+        $this->group = [];
+        $this->query = '';
     }
-
 
     public function setTable($table)
     {
-        $this->table = $table;
+        $table = trim($table); 
+        $this->tables[] = $table;
         return $this;
     }
 
-    /**
-     * @param $fields
-     * @return $this
-     */
-    public function setField($fields)
+    public function setField($table, $field)
     {
-        if(is_array($fields)){
-            foreach ($fields as $field) {
-                $this->fields[] = $field;
-            }
-        }else{
-            $this->fields = $fields;
+        if (in_array($table, $this->tables)) {
+            $this->fields[$field] = $table;
         }
         return $this;
     }
 
 
-    public function setQuery($query)
-    {
-        $this->query = $query;
+    public function setJoin($join = 'INNER JOIN', $primeTable, $primeFild, $joinTable, $joinField){
+        if($join == 'INNER JOIN' || $join == 'LEFT JOIN' || $join == 'RIGHT JOIN' || $join == 'FULL JOIN') {
+            if (in_array($primeTable, $this->tables)) {
+                if (in_array($joinTable, $this->tables)) {
+                    $this->joins[] = ['joinType' => $join, 'primeTable' => $primeTable, 'primeFild' => $primeFild, 'joinTable' => $joinTable, 'joinField' => $joinField ];
+                    return $this;
+                } else {
+                    return $this;
+                }
+            } else {
+                return $this;
+            }
+        }
+    }
+
+        
+    public function setWhere($field = 'author_id', $operator = '=', $value = '13', $separator = 'OR', $table = false) {
+        $validOperators = ['=', '>', '<', '>=', '<=', '<>', 'BETWEEN', 'LIKE', 'IN'];
+        $validSeparators = ['AND', 'OR', 'NOT'];
+        if (in_array($operator, $validOperators)) {
+            $this->conditions[] = ['field' => $field, 'operator' => $operator, 'value' => $value];
+            if (in_array($separator, $validSeparators)) {
+                $this->conditions[count($this->conditions)-1]['separator'] = $separator;
+            }
+            if ($table) {
+                $this->conditions[count($this->conditions)-1]['table'] = $table;
+            }
+        }
+        return $this;
+
+    }
+
+    /*GROUP BY books.id";*/
+
+    public function setGroupBy($table, $field) {
+        if (in_array($table, $this->tables)) {
+            $this->group = [$table, $field];
+        }
         return $this;
     }
 
-    public function insertRow() {
-
-    }
 }
